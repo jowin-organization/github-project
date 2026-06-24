16413cc5ef489bf1933be8bbce77d56bc360a6d3
jowin209 <jowinlarisma@proton.me>
2026-06-24 21:49:26 +0800

    Add useState and create function for card handlers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  function showCardHandler() {
+    setCartIsShown(true);
+  }
+
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
+
   return (
     <CartProvider>
       <Header />
